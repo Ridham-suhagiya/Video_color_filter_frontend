5e381aaa1dc04d05be8bfd031945296b0252d166
ridham <ridhamsuhagiya@gmail.com>
2023-01-11 22:44:40 +0530

    video frontend preview done

--- color_filter/src/components/Select_form.js
+++ color_filter/src/components/Select_form.js
@@ -4,9 +4,16 @@ import  placeholder from "../assets/placeholder.gif";
 
 const Select_form = () => {
 
-	function get_image(e){
+	function get_file(e){
 	    let url = URL.createObjectURL(e.target.files[0])
-		document.getElementById('prev_image').setAttribute('src',url);			
+	    if (e.target.files[0].type.split('/')[0] == 'video'){
+	    	document.getElementById('prev_file').setAttribute('src',url);
+	    	document.getElementById('prev_file').style.display = 'block';
+	    	document.getElementById('prev_image').style.display = 'none';
+	    }
+		else{   
+		    document.getElementById('prev_image').setAttribute('src',url);			
+		}
 	}
 
 	function get_input() {
@@ -26,14 +33,11 @@ const Select_form = () => {
 					body: JSON.stringify({"file": e.target.result})
 				}
 			)
-			.then(() => console.log('hithua'));	
+			.then((resp) => resp.json())
+			.then((data) => console.log(data))	
 	  	}
   		reader.readAsDataURL(img);
-
-		
 	}
-
-	
 
 	return (
 		<div className='form' >
@@ -44,11 +48,13 @@ const Select_form = () => {
 				<form method='POST' id='file-form' encType='multipart/form-data'>
 					<div className='preview-file' onClick={get_input}>
 						<img className="prev_image" src={placeholder} alt=''  id='prev_image'/>
+						<video className="prev_file"  alt=''  id='prev_file' autoPlay={true} muted={true}>
+						</video>
 					</div>
 					<div className='input-section'>
 						{/*<label>
 							Add file here*/}
-							<input type='file' name='file' className='file-input-box' onChange={get_image} id='file_input'/>
+							<input type='file' name='file' className='file-input-box' onChange={get_file} id='file_input'/>
 						{/*</label>*/}
 					</div>
 					<div className='button-cover'>
