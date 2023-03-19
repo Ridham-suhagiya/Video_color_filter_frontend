b2e99dd12aadca2b3c4e6057f19f63d5a8154283
ridham <ridhamsuhagiya@gmail.com>
2023-03-19 13:45:50 +0530

    changes

--- color_filter/src/components/Select_form.js
+++ color_filter/src/components/Select_form.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './components.css';
 import  placeholder from "../assets/placeholder.gif";
 import { useState, useEffect } from 'react';
-const selectForm = (file_byte_setter) => {
+const SelectForm = (file_byte_setter) => {
 	const [new_bytes , new_byte_setter] = useState(null);	
 	const [file_url, file_url_setter] = useState(null);
 
@@ -165,4 +165,4 @@ const selectForm = (file_byte_setter) => {
 	)
 }
 
-export default Select_form
+export default SelectForm;
